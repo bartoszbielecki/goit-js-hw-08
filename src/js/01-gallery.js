b7d8ddaf091a6be60d1cae10cbe23499523e437a
Bartosz Bielecki <bartosz.bielecki@outlook.com>
2024-05-03 16:38:56 +0200

    Update files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
+
+  galleryItems.forEach(item => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    const image = document.createElement('img');
+
+    link.classList.add('gallery__item');
+    link.href = item.original;
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+
+    link.appendChild(image);
+    listItem.appendChild(link);
+    gallery.appendChild(listItem);
+  });
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
