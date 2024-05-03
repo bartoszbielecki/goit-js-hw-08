3f1dd797d907e6f03fd3eea91478046438c04f60
Bartosz Bielecki <bartosz.bielecki@outlook.com>
2024-05-03 16:32:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('form.feedback-form');
 
+const formDataStr = localStorage.getItem('feedback-form-state');
+if (formDataStr) {
+  const formData = JSON.parse(formDataStr);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', ev => {
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+});
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  console.log({
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+  localStorage.clear();
+  form.reset();
+});
