84a12b8b8bda46bce25d454bd2fe2bd318f74675
Bartosz Bielecki <bartosz.bielecki@outlook.com>
2024-05-03 16:13:55 +0200

    Update files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(Number.parseFloat(currentTime));
+}
+
+const delayedUpdate = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', eventData => {
+  delayedUpdate(eventData);
+});
